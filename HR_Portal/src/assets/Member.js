d24d285a3b83d9a057cdf09282f4cda90a986cf6
Acidix <douglasgbays@yahoo.com>
2025-10-20 07:28:00 -0700

    set up css pages and jsx pages and a nav bar

--- HR_Portal/src/assets/Member.js
+++ HR_Portal/src/assets/Member.js
@@ -1,6 +1,7 @@
 const Members = {
     humanResources: [{
         name: 'Doulgas Bays', 
+        username: 'douglas.bays',
         title: 'Human Resources Advisor',
         onboarded: True,
         approvesVacation: True
@@ -9,6 +10,7 @@ const Members = {
     coaches: [
         {
             name: 'Andy Reid',
+            username: 'andy.reid',
             title: 'Head Coach',
             onboarded: True,
             approvesVacation: True
@@ -17,6 +19,7 @@ const Members = {
     quaterbacks: [
         {
         name: 'Patrick Mahomes', 
+        username: 'patrick.mahomes',
         title: 'Quarterback',
         onboarded: True,
         approvesVacation: False
@@ -25,6 +28,7 @@ const Members = {
     wideRecievers: [
         {
         name: 'Xavier Worthy', 
+        username: 'xavier.worthy',
         title: 'Wide Reciever',
         onboarded: True,
         approvesVacation: False
