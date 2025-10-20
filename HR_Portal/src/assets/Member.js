284e4f2d9da7a2f0cf89ea0b8e2f7b151d8d02d8
Acidix <douglasgbays@yahoo.com>
2025-10-19 19:35:32 -0700

    creating members

--- HR_Portal/src/assets/Member.js
+++ HR_Portal/src/assets/Member.js
@@ -1,8 +1,35 @@
-export function Members [
-    User {
-        Name: Douglas Bays,
-        Title: Human Resources,
-        Onboarded: Yes 
-    },
-    
-];
+const Members = {
+    humanResources: [{
+        name: 'Doulgas Bays', 
+        title: 'Human Resources Advisor',
+        onboarded: True,
+        approvesVacation: True
+    }
+    ],
+    coaches: [
+        {
+            name: 'Andy Reid',
+            title: 'Head Coach',
+            onboarded: True,
+            approvesVacation: True
+        }
+    ],
+    quaterbacks: [
+        {
+        name: 'Patrick Mahomes', 
+        title: 'Quarterback',
+        onboarded: True,
+        approvesVacation: False
+    }
+    ], 
+    wideRecievers: [
+        {
+        name: 'Xavier Worthy', 
+        title: 'Wide Reciever',
+        onboarded: True,
+        approvesVacation: False
+    }
+    ],
+};
+
+export default Members;
